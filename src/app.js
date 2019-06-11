4fe284511473d8d9070375b68a9be175a62ad722
Harshit Pareek <hp1014@nyu.edu>
2019-06-10 22:44:51 -0700

    feat(deploying on heroku)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+// dynamically finding the PORT on heroku
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('<h1>Node is Best!!</h1>');
